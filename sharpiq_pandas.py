f8169eee5fb567bb6851127ad3b0daa8fb2bc9bf
SharpIQAnalytics <sharpiq@gmail.com>
2026-06-29 19:33:08 +1000

    remove debug prints,clean up output

--- sharpiq_pandas.py
+++ sharpiq_pandas.py
@@ -1,16 +1,17 @@
 import pandas as pd
 df=pd.read_csv('games.csv')
-print(df)
+
 df['total']=1/df['home_odds']+1/df['away_odds']
-print(df)
+
 df['fair_home']=df['home_odds']*df['total']
 df['fair_away']=df['away_odds']*df['total']
-print(df)
+
 df['edge_home']=df['best_price_home']/df['fair_home']-1
 df['edge_away']=df['best_price_away']/df['fair_away']-1
-print(df)
+
 print(df[df['edge_home']>0][['home_team','edge_home']])
 print(df[df['edge_away']>0][['away_team','edge_away']])
 print(df.sort_values('edge_home',ascending=False)[['home_team','edge_home']])
-print(df.sort_values('edge_away',ascending=False)[['away_team','edge_away']])
+print(df.sort_values('edge_away',ascending=False)[['away_team','edge_away']])      
+      
 df.to_csv('sharpiq_results.csv')
