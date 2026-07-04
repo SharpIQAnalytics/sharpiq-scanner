1aa9ba22d4eb09a622d3502274a31d39b650c638
SharpIQAnalytics <sharpiq@gmail.com>
2026-07-05 09:30:55 +1000

    fix:remove DELETE FROM games,clean database structure

--- sharpiq_database.py
+++ sharpiq_database.py
@@ -1,7 +1,7 @@
 import sqlite3
 conn=sqlite3.connect('sharpiq.db')
 cursor=conn.cursor()
-cursor.execute(''' 
+cursor.execute('''
                CREATE TABLE IF NOT EXISTS games(
                date TEXT,
                home_team TEXT,
@@ -14,40 +14,9 @@ cursor.execute('''
                closing_home REAL,
                closing_away REAL)
                ''')
-cursor.execute('DELETE FROM games')
-cursor.execute('''
-               INSERT INTO games
-               (date,home_team,away_team,home_odds,away_odds,best_price_home,best_price_away,result,closing_home,closing_away)
-               VALUES('2026-06-28','Newcastle','West Tigers',1.44,2.80,1.50,2.90,'home',1.35,3.70)
-               ''')
-cursor.execute('''
-               INSERT INTO games
-               (date,home_team,away_team,home_odds,away_odds,best_price_home,best_price_away,result,closing_home,closing_away)
-               VALUES('2026-07-03','Penrith Panthers','South Sydney Rabbitohs',1.77,2.00,1.83,2.15,NULL,NULL,NULL)
-               ''')
-cursor.execute('''
-               INSERT INTO games
-               (date,home_team,away_team,home_odds,away_odds,best_price_home,best_price_away,result,closing_home,closing_away)
-               VALUES('2026-07-04','Brisbane Broncos','Cronulla Sharks',2.47,1.55,2.55,1.55,NULL,NULL,NULL)
-               ''')
-cursor.execute('''
-               INSERT INTO games
-               (date,home_team,away_team,home_odds,away_odds,best_price_home,best_price_away,result,closing_home,closing_away)
-               VALUES('2026-07-05','Parramatta Eels','Manly Warringah',1.35,3.20,1.40,3.22,NULL,NULL,NULL)
-               ''')
-cursor.execute('''
-                INSERT INTO games
-                (date,home_team,away_team,home_odds,away_odds,best_price_home,best_price_away,result,closing_home,closing_away)
-                VALUES('2026-07-05','Newcastle Knights','Dolphins',2.19,1.65,2.30,1.70,NULL,NULL,NULL)
-                ''')
 conn.commit()
-cursor.execute('''
-               UPDATE games
-               SET result = NULL,closing_home = NULL,closing_away = NULL
-               WHERE home_team = "Penrith Panthers"
-               ''')
+cursor.execute('DELETE FROM games WHERE rowid NOT IN (SELECT MIN(rowid)FROM games GROUP BY home_team)')
 conn.commit()
-cursor.execute('SELECT home_team,away_team,date,home_odds FROM games ORDER BY date ASC,home_odds DESC')
-cursor.execute('SELECT COUNT (*) FROM games WHERE result IS NULL')
+cursor.execute('SELECT home_team,away_team,result,closing_home,closing_away FROM games')
 for row in cursor.fetchall():
     print(row)
